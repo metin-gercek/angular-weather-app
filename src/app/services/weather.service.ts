9ecb3aa61388ca3edeff840dd16974a5ab7531c4
metin-gercek <metin.gercek@edu.bc.fi>
2022-03-25 15:07:14 +0200

    change http to https

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -10,7 +10,7 @@ export class WeatherService {
   URI: string = '';
 
   constructor(private http: HttpClient) {
-    this.URI = `http://api.openweathermap.org/data/2.5/find?q=`;
+    this.URI = `https://api.openweathermap.org/data/2.5/find?q=`;
 
   }
 
